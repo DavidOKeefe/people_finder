5c0dee92fa137d11ce08e2aa34b638c30f6c6842
David OKeefe <davidpokeefe3@gmail.com>
2018-07-08 15:35:10 -0400

    Add class method to person to determine email address character frequency

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,15 @@
 class Person < ApplicationRecord
+
+  def self.email_address_character_count
+    email_addresses = self.all.pluck(:email)
+
+    results = email_addresses.each_with_object({}) do |email, collection|
+      email.split('').each do |letter|
+        collection[letter] = 0 unless collection[letter].present?
+        collection[letter] += 1
+      end
+    end
+
+    Hash[results.sort_by{ |_k, v| -v }]
+  end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,4 +1,40 @@
 require 'rails_helper'
 
 describe Person do
+  describe '.email_address_character_count' do
+    let(:email_1) { 'test@test.com' }
+    let(:email_2) { 'another_address@gmail.com' }
+    let(:email_addresses) { [email_1, email_2] }
+    let(:expected_results) do
+      {
+        't' => 5,
+        'e' => 4,
+        's' => 4,
+        'a' => 3,
+        'o' => 3,
+        'm' => 3,
+        'c' => 2,
+        '@' => 2,
+        'r' => 2,
+        'd' => 2,
+        '.' => 2,
+        'g' => 1,
+        '_' => 1,
+        'l' => 1,
+        'i' => 1,
+        'n' => 1,
+        'h' => 1
+      }
+    end
+
+    before do
+      allow(described_class)
+        .to receive_message_chain(:all, :pluck)
+        .and_return(email_addresses)
+    end
+
+    it 'returns character frequency counts' do
+      expect(described_class.email_address_character_count).to eq(expected_results)
+    end
+  end
 end
